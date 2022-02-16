4647c92ce64b08e14e16c473d364cd250b254eb0
Mariano Carlos Silva <Mariano.silva@ufvjm.edu.br>
2022-02-16 19:33:53 -0300

    Adicionar arquivos de projeto.

---
+++ 30 Base de dados/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace _30_Base_de_dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.lista = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnCriarTabela = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.btnExcluie = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(20, 9);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(580, 82);
+            this.labelResultado.TabIndex = 0;
+            this.labelResultado.Text = "Resultado";
+            // 
+            // lista
+            // 
+            this.lista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.lista.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.lista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nome,
+            this.email});
+            this.lista.Location = new System.Drawing.Point(25, 181);
+            this.lista.MultiSelect = false;
+            this.lista.Name = "lista";
+            this.lista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.lista.Size = new System.Drawing.Size(544, 308);
+            this.lista.TabIndex = 1;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "NOME";
+            this.nome.Name = "nome";
+            // 
+            // email
+            // 
+            this.email.HeaderText = "EMAIL";
+            this.email.Name = "email";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(580, 69);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(580, 69);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Email";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(112, 62);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(460, 35);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(112, 103);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(460, 35);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(620, 62);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(140, 51);
+            this.btnConectar.TabIndex = 6;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnCriarTabela
+            // 
+            this.btnCriarTabela.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarTabela.Location = new System.Drawing.Point(620, 137);
+            this.btnCriarTabela.Name = "btnCriarTabela";
+            this.btnCriarTabela.Size = new System.Drawing.Size(140, 51);
+            this.btnCriarTabela.TabIndex = 7;
+            this.btnCriarTabela.Text = "Criar Tabela";
+            this.btnCriarTabela.UseVisualStyleBackColor = true;
+            this.btnCriarTabela.Click += new System.EventHandler(this.btnCriarTabela_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(620, 212);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(140, 51);
+            this.btnInserir.TabIndex = 8;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcurar.Location = new System.Drawing.Point(620, 287);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(140, 51);
+            this.btnProcurar.TabIndex = 9;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // btnExcluie
+            // 
+            this.btnExcluie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluie.Location = new System.Drawing.Point(620, 362);
+            this.btnExcluie.Name = "btnExcluie";
+            this.btnExcluie.Size = new System.Drawing.Size(140, 51);
+            this.btnExcluie.TabIndex = 10;
+            this.btnExcluie.Text = "Excluir";
+            this.btnExcluie.UseVisualStyleBackColor = true;
+            this.btnExcluie.Click += new System.EventHandler(this.btnExcluie_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(620, 438);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(140, 51);
+            this.btnEditar.TabIndex = 11;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 566);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnExcluie);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnCriarTabela);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.labelResultado);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.DataGridView lista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnCriarTabela;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Button btnExcluie;
+        private System.Windows.Forms.Button btnEditar;
+    }
+}
+
---
+++ 30 Base de dados/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Import dpo MYSQL
+using MySql.Data.MySqlClient;
+
+namespace _30_Base_de_dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            #region MYSQL
+
+            /*string de conexão serve para linkar o projeto ao bd, na 2, eu ja tenho um banco
+             * na 1 eu aidna não tenho. */
+
+            string strconnetion1 = "server=127.0.0.1; User Id=root;password=";
+            //string strConnection2 = "server=127.0.0.1; User Id=root;database=curso_db;password=";
+
+            //CRIANDO A CONEXÃO COM O BD
+
+            MySqlConnection conexão = new MySqlConnection(strconnetion1);
+
+            try
+            {
+                conexão.Open();
+                labelResultado.Text = "Conectado MySQL";
+
+                //Fazer comandos sql dentro do c# no meu local host phpmyadmin
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexão;
+                comando.CommandText = "CREATE DATABASE IF NOT EXISTS curso_db";
+                comando.ExecuteNonQuery();
+                labelResultado.Text = "Base de dados criada com sucesso";
+
+                comando.Dispose(); // limpar o comando dps que ele for executado
+
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Erro ao conectar MySQL";
+
+            }
+            finally
+            {
+                
+                conexão.Close();
+            }
+            // TESTAR SE O BD EXSTE, CASO NÃO EXISTE, VOU CRIÁ-LA NO SERVIDOR, VOU FAZER ISSO DENTRO DO TRY, NA LINHA 36
+                
+
+            #endregion
+        }
+
+        private void btnCriarTabela_Click(object sender, EventArgs e)
+        {
+            string strConnection = "server=127.0.0.1; User Id=root;database=curso_db;password="; // string de conexao com bd
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+            try
+            {
+                conexao.Open();
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+                comando.CommandText = "CREATE TABLE pessoas ( id INT NOT NULL , nome VARCHAR(50), email VARCHAR(60), PRIMARY KEY(id))";
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Tabela Criada com sucesso.";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                labelResultado.Text = ex.Message;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+
+            string strConnection = "server=127.0.0.1; User Id=root;database=curso_db;password="; // string de conexao com bd
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            int id = new Random(DateTime.Now.Millisecond).Next(0, 1000);
+            string nome = txtNome.Text;
+            string email = txtEmail.Text;
+
+            try
+            {
+                conexao.Open();
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+                comando.CommandText = "INSERT INTO pessoas VALUES(" + id + ", '" + nome + "','" + email + "')";
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "registro inserido com sucesso";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                labelResultado.Text = ex.Message;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            string strConnection = "server=127.0.0.1; User Id=root;database=curso_db;password="; // string de conexao com bd
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            try
+            {
+                string query = "SELECT * FROM pessoas";
+                if(txtNome.Text != "")
+                {
+                    query = "SELECT * FROM pessoas WHERE nome LIKE'" + txtNome.Text+"'";
+                }
+
+                DataTable dados = new DataTable();
+
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(query, strConnection);
+
+                conexao.Open();
+                adaptador.Fill(dados);
+
+                foreach (DataRow linha in dados.Rows)
+                {
+                    lista.Rows.Add(linha.ItemArray);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                lista.Rows.Clear();
+                labelResultado.Text = ex.Message;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+
+
+        }
+
+        private void btnExcluie_Click(object sender, EventArgs e)
+        {
+            
+            string strConnection = "server=127.0.0.1; User Id=root;database=curso_db;password="; // string de conexao com bd
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+
+            
+            try
+            {
+                conexao.Open();
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+
+                int id = (int)lista.SelectedRows[0].Cells[0].Value;
+
+                comando.CommandText = "DELETE FROM pessoas WHERE id = '"+id+"'";// o (int) no inicio foi para realizar uma conversao explicita para int
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Registro excluido com sucesso";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                labelResultado.Text = ex.Message;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            string strConnection = "server=127.0.0.1; User Id=root;database=curso_db;password="; // string de conexao com bd
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+
+
+            try
+            {
+                conexao.Open();
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+
+                int id = (int)lista.SelectedRows[0].Cells[0].Value;// o (int) no inicio foi para realizar uma conversao explicita para int
+                string nome = txtNome.Text;
+                string email = txtEmail.Text;
+                string query = "UPDATE pessoas SET nome = '" + nome + "', email = ' " + email + " ' WHERE id LIKE '" + id + "'";
+
+
+
+                comando.CommandText = query;
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Registro alterado com sucesso";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                labelResultado.Text = ex.Message;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
